614d98b08bd16d5d315e5d07649128a4b964b845
aliburhankeskin <keskinciali@hotmail.com>
2021-07-09 01:57:35 +0300

    Added react-native-modal package and FancyModal Component

---
+++ src/components/global/FancyModal.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Modal from 'react-native-modal';
+import Color from '@assets/color';
+
+function FancyModal({
+  children, isVisible, onBackdropPress 
+}) {
+
+  return (
+    <Modal
+      isVisible={isVisible}
+      backdropColor={Color.primary}
+      backdropOpacity={0.8}
+      animationIn="zoomInDown"
+      animationOut="zoomOutUp"
+      animationInTiming={200}
+      animationOutTiming={200}
+      backdropTransitionInTiming={200}
+      backdropTransitionOutTiming={200}
+      onBackdropPress={onBackdropPress}
+    >
+      {children}
+    </Modal>
+  );
+
+}
+export default FancyModal;
