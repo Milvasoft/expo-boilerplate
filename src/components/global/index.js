be83a1544f7ba88cd46f27b4b4869c9a4e44e9d7
aliburhankeskin <keskinciali@hotmail.com>
2021-07-09 01:59:44 +0300

    Added Global Component index.jsx and DoubleClicker

---
+++ src/components/global/index.js
@@ -0,0 +1,13 @@
+import SafeAreaComponent from './SafeAreaComponent';
+import GeneralActivityIndicator from './GeneralActivityIndicator';
+import DoubleClicker from './DoubleClicker';
+import FancyModal from './FancyModal';
+import PageHaeder from './PageHeader';
+
+export {
+  SafeAreaComponent,
+  GeneralActivityIndicator,
+  DoubleClicker,
+  FancyModal,
+  PageHaeder
+};
