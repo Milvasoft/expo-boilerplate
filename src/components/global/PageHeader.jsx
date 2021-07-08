3ce1dc1230508889a3adc914a4f291e465b8e77f
aliburhankeskin <keskinciali@hotmail.com>
2021-07-09 01:58:28 +0300

    Added PageHaeder Component

---
+++ src/components/global/PageHeader.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  StyleSheet, TouchableWithoutFeedback, View, Text 
+} from 'react-native';
+import { MaterialIcons, } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import Color from '@assets/color';
+import { hp, wp } from '@helpers/responsive';
+
+export default function PageHaeder({ title }) {
+
+  const navigation = useNavigation();
+
+
+  const _goback = () => {
+
+    navigation.goBack();
+      
+  };  
+    
+  return (
+    <View style={Styles.root}>
+
+      <View style={Styles.goBackIcon}>
+        <TouchableWithoutFeedback onPress={_goback}>           
+          <MaterialIcons
+            name="arrow-back-ios"
+            size={wp(7)}
+            color={Color.primary}
+          />
+        </TouchableWithoutFeedback>          
+      </View>
+
+      <Text style={Styles.title}>{title}</Text> 
+
+      <View />
+
+    </View>
+  );
+
+}
+
+const Styles = StyleSheet.create({
+  root: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: hp(8),
+    borderBottomColor: Color.primary,
+    borderBottomWidth: 1,
+  },
+
+  goBackIcon: {
+    paddingLeft: wp(3),        
+  },
+
+  title: {
+    color: Color.primary,
+    fontSize: wp(6),
+    paddingRight: wp(3),        
+    fontWeight: 'bold',
+  }
+});
+      
