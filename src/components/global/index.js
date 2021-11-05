d2febde37956423456241620d8a395ce510920c6
Ali Burhan Keskin <32386753+aliburhankeskin@users.noreply.github.com>
2021-11-05 19:01:56 +0300

    Unnecessary components removed

--- src/components/global/index.js
+++ src/components/global/index.js
@@ -1,16 +1,8 @@
 /**
  * @author Ali Burhan Keskin <alikeskin@milvasoft.com>
  */
-import SafeAreaComponent from './SafeAreaComponent';
 import GeneralActivityIndicator from './GeneralActivityIndicator';
-import DoubleClicker from './DoubleClicker';
-import FancyModal from './FancyModal';
-import PageHaeder from './PageHeader';
 
 export {
-  SafeAreaComponent,
   GeneralActivityIndicator,
-  DoubleClicker,
-  FancyModal,
-  PageHaeder
 };
