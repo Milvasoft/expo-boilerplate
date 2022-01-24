e239e7fa26febf5c429bb04018e3be3616be0fa4
Ali Burhan Keskin <32386753+aliburhankeskin@users.noreply.github.com>
2022-01-24 22:05:17 +0300

    Paper Bug fix

--- src/providers/Paper.tsx
+++ src/providers/Paper.tsx
@@ -1,28 +1,31 @@
+import { useTheme } from '@src/hooks';
 import * as React from 'react';
 import { Provider, DefaultTheme } from 'react-native-paper';
-import Color from '@assets/color';
 
-const theme = {
-  ...DefaultTheme,
-  dark: false,
-  roundness: 2,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: Color().primary,  
-    accent: '#ffffff',
-    background: '#ffffff',
-    surface: '#ffffff',
-    text: '#000000',
-    placeholder: Color().primary
-
-  },
-};
   
  type Props = {
    children: React.ReactNode
  };
 
 function PaperProvider({ children } : Props) {
+
+  const customTheme = useTheme();
+
+  const theme = {
+    ...DefaultTheme,
+    dark: false,
+    roundness: 2,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: customTheme.primary,  
+      accent: '#ffffff',
+      background: '#ffffff',
+      surface: '#ffffff',
+      text: '#000000',
+      placeholder: customTheme.primary
+  
+    },
+  };
 
   return (
     <Provider theme={theme}>
