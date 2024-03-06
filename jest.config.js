efb10971f249117795517ed09b7bffbd4d02da77
Ali Burhan Keskin <32386753+aliburhankeskin@users.noreply.github.com>
2024-03-06 23:32:47 +0300

    jest config

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  verbose: true,
+  preset: "jest-expo",
+  transformIgnorePatterns: [
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|react-redux|native-base|react-native-svg)",
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.{js,jsx}",
+    "!**/coverage/**",
+    "!**/node_modules/**",
+    "!**/babel.config.js",
+    "!**/jest.setup.js",
+  ],
+};
