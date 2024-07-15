ac85b324b9faf6c3f44bf054ab635e8a2c44bd8d
Ali Burhan Keskin <32386753+aliburhankeskin@users.noreply.github.com>
2024-07-15 13:51:45 +0300

    feat: expo upgrade 51

--- app.config.ts
+++ app.config.ts
@@ -1,57 +1,54 @@
-import { ExpoConfig, ConfigContext } from '@expo/config'
+import { ExpoConfig, ConfigContext } from "@expo/config";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: 'Milvasoft Expo BoilerPalte',
-  description: 'Milvasoft Expo BoilerPalte Description',
-  slug: 'milvasoft-expo-boilerplate',
-  scheme: 'com.milvasoft.expoboilerplate',
-  version: '1.0.0',
-  sdkVersion: '50.0.0',
-  orientation: 'portrait',
-  icon: './src/assets/images/icon.png',
-  userInterfaceStyle: 'automatic',
+  name: "Milvasoft Expo BoilerPalte",
+  description: "Milvasoft Expo BoilerPalte Description",
+  slug: "milvasoft-expo-boilerplate",
+  scheme: "com.milvasoft.expoboilerplate",
+  version: "1.0.0",
+  sdkVersion: "51.0.0",
+  orientation: "portrait",
+  icon: "./src/assets/images/icon.png",
+  userInterfaceStyle: "automatic",
   runtimeVersion: {
-    policy: "sdkVersion"
+    policy: "sdkVersion",
   },
-  assetBundlePatterns: [
-    './src/assets/images/*'
-  ],
+  assetBundlePatterns: ["./src/assets/images/*"],
   locales: {
-    tr: './src/assets/languages/turkish.json',
-    en: './src/assets/languages/english.json'
+    tr: "./src/assets/languages/turkish.json",
+    en: "./src/assets/languages/english.json",
   },
   splash: {
-    image: './src/assets/images/splash.png',
-    resizeMode: 'contain',
-    backgroundColor: '#ffffff'
+    image: "./src/assets/images/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
   },
   ios: {
-    bundleIdentifier: 'com.milvasoft.expoboilerplate',
-    buildNumber: '1.0.0',
+    bundleIdentifier: "com.milvasoft.expoboilerplate",
+    buildNumber: "1.0.0",
     infoPlist: {
-      CFBundleAllowMixedLocalizations: true
-    }
+      CFBundleAllowMixedLocalizations: true,
+    },
   },
   web: {
-    bundler: "metro"
+    bundler: "metro",
   },
   android: {
     adaptiveIcon: {
-      foregroundImage: './src/assets/images/adaptive-icon.png',
-      backgroundColor: '#ffffff'
+      foregroundImage: "./src/assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff",
     },
-    package: 'com.milvasoft.expoboilerplate',
+    package: "com.milvasoft.expoboilerplate",
     versionCode: 1,
   },
   updates: {
     enabled: true,
-    url: "https://u.expo.dev/49e4e24d-c928-4ff1-815d-f1a58ca580bd"
+    url: "https://u.expo.dev/49e4e24d-c928-4ff1-815d-f1a58ca580bd",
   },
-  extra:{
-    eas:{
-      projectId: "49e4e24d-c928-4ff1-815d-f1a58ca580bd"
-    }
-  }
-
-})
+  extra: {
+    eas: {
+      projectId: "49e4e24d-c928-4ff1-815d-f1a58ca580bd",
+    },
+  },
+});
