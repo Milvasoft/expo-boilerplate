9a44a86b112fb18eb8d2b3623fe11bdd9cf65edc
Ali Burhan Keskin <32386753+aliburhankeskin@users.noreply.github.com>
2024-07-20 15:07:30 +0300

    fix: appslice

--- src/modules/app/redux/appSlice.ts
+++ src/modules/app/redux/appSlice.ts
@@ -1,48 +1,41 @@
-import { IAppReducer } from '@modules/app/redux/IAppReducer';
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-
+import { IAppReducer } from "@modules/app/redux/IAppReducer";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 const initialState: IAppReducer = {};
 
 export const appSlice = createSlice({
-  name: 'appReducer',
+  name: "appReducer",
   initialState,
   reducers: {
-    setAuthToken:(state, action: PayloadAction<any>) => {
+    setAuthToken: (state, action: PayloadAction<any>) => {
       state = { ...state, authToken: action.payload };
+      return state;
     },
     SetUser: (state, action: PayloadAction<any>) => {
-
       state = { ...state, isSignedIn: true, user: action.payload };
-    
+      return state;
     },
     LoggedOut: (state) => {
-
-      state = { ...state, user :null, isSignedIn: false, authToken: ""};
-
+      state = { ...state, user: null, isSignedIn: false, authToken: "" };
       return state;
-    
     },
     SetColorShceme: (state, action: PayloadAction<any>) => {
-        
       state = { ...state, userColorScheme: action.payload };
-      
+      return state;
     },
     SetExpoToken: (state, action: PayloadAction<any>) => {
-        
       state = { ...state, expoToken: action.payload };
-      
-    }
-
+      return state;
+    },
   },
 });
 
-export const { 
+export const {
   setAuthToken,
   SetUser,
   LoggedOut,
   SetColorShceme,
-  SetExpoToken
+  SetExpoToken,
 } = appSlice.actions;
 
 export default appSlice.reducer;
